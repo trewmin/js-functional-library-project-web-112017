16c5289b6b12a075c984cadf9f74209628fb0554
trewmin <trumanlahr@gmail.com>
2018-01-09 15:42:44 -0500

    did each and map, passing to matt

--- index.js
+++ index.js
@@ -4,18 +4,38 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, iteratee) {
+      if (Array.isArray(collection)) {
+        for (let i = 0; i < collection.length; i++) {
+          iteratee(collection[i], i, collection)
+        }}
+      else {
+        for (const element in collection) {
+          iteratee(collection[element], element, collection)
+        }};
     },
 
-    map: function() {
-
+    map: function(collection, iteratee) {
+      if (Array.isArray(collection)) {
+        const newCollection = []
+        for (let i = 0; i < collection.length; i++) {
+          newCollection.push(iteratee(collection[i], i, collection))
+        }
+        return newCollection
+      }
+      else {
+        const newCollection = {}
+        for (const element in collection) {
+          newCollection[element] = iteratee(collection[element], element, collection)
+        }
+        return newCollection
+      };
     },
 
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
